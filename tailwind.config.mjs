4c228be634d33574a0278949eba90a3e8bdf256f
Juan José Morales <deddomanwandarlando@gmail.com>
2025-03-11 13:37:53 -0300

    Add Bio and Skills sections, Navbar, and update Tag index layout. Installed tailwindcss typography plugin. Reworked blog and layouts.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
-		extend: {},
+	  extend: {
+		typography: {
+		  DEFAULT: {
+			css: {
+			  maxWidth: '100%',
+			},
+		  },
+		},
+	  },
 	},
-	plugins: [],
-}
+	plugins: [require('@tailwindcss/typography')],
+  }

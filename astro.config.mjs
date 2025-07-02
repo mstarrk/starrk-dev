8a0e7650056abcd4ec0492047037bd434e61a4cb
Juan José Morales <deddomanwandarlando@gmail.com>
2025-07-02 12:53:32 -0300

    feat: update dependencies - Added @iconify-json/mdi and astro-icon to package.json. - Simplified the Footer component's. - SocialIconLink component to use Iconify for rendering icons instead of inline SVG.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,10 @@ import tailwind from '@astrojs/tailwind';
 
 import preact from '@astrojs/preact';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://starrk.dev",
-  integrations: [tailwind(), preact()]
+  integrations: [tailwind(), preact(), icon()]
 });
